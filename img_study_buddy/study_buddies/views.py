96411d9d0de44abba0f490a8d5e67e6903dcf3bb
poppykode <ngonimug@gmail.com>
2023-07-05 07:54:11 +0200

    add study buddies urls and template files

--- img_study_buddy/study_buddies/views.py
+++ img_study_buddy/study_buddies/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
+def view_all_study_buddies(request):
+    template_name = 'view_all_study_buddies.html'
+    return render(request,template_name)
